ed4abe6857af6e8d809f7f673aedddc99c781cca
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-05-11 16:35:57 +0530

    made a state in APP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import Todos from './components/Todos'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>TODO APP</h1>
-    </div>
-  );
+class App extends React.Component{
+  state={
+    todos:[
+      {
+        id:1,
+        title:'learn basic',
+        completed:false
+      },
+      {
+        id:2,
+        title:'learn language',
+        completed:false
+      },
+      {
+        id:3,
+        title:'learn technology',
+        completed:false
+      }
+    ]
+  }
+  render(){
+
+    return (
+      <div className="App">
+        <Todos/>
+  
+      </div>
+    );
+  }
 }
+
 
 export default App;
